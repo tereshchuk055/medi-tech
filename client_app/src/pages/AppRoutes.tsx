6b3fcd71e7c1ef05cc71cbc8d93f57584db284d3
ipz214_shchss <ipz214_shchss@student.ztu.edu.ua>
2023-11-25 21:53:51 +0200

    Added routes

--- client_app/src/pages/AppRoutes.tsx
+++ client_app/src/pages/AppRoutes.tsx
@@ -1,18 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import { NotFound } from './NotFound';
 import { Home } from './Home';
-
+import RootLayout from '../components/Layout/Layout';
 import LoginForm from '../components/LoginForm/LoginForm';
 import RegisterForm from '../components/RegisterForm/RegisterForm';
 import TreatmentForm from '../components/TreatmentForm/TreatmentForm';
-
+import News from './News';
 export const AppRoutes = () => {
 
     return (
         <Routes>
             <Route path="/signIn" element={<LoginForm />} />
             <Route path="/signUp" element={<RegisterForm />} />
-            
+            <Route path="/" element={<RootLayout />}>
+                <Route index element={<Home />} />
+                <Route path="/addTreat" element={<TreatmentForm />} />
+                <Route path="/news" element={<News />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
         </Routes>
     )
 }
