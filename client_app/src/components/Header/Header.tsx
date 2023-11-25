698426749c042eee6b6894fc5b14d0bc2a12f788
SofiaHorai <sofia.113.ya@gmail.com>
2023-11-25 21:13:10 +0200

    added layout

--- client_app/src/components/Header/Header.tsx
+++ client_app/src/components/Header/Header.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import { useState } from 'react';
 import Logo from '../Logo/Logo';
 
+
 export default function Header() {
+    const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
+
     return (
         <header className="bg-neutral-900 pr-5  xl:border-b border-gray-200 border-opacity-75 border-gray-100 w-full ">
             <nav>
@@ -11,17 +14,25 @@ export default function Header() {
                         <div className="flex space-x-12 items-center text-lg text-sky-100 ">
                             <li><a href="#">Головна</a></li>
                             <li><a href="#" >Про нас</a></li>
-                            <li><a href="#">Послуги</a></li>
-                            <li><a href="#">Медичні заклади</a></li>
-                            
+                            <li><a href="/Employees">Співробітники</a></li>
+                            <li><a href="/News">Новини</a></li>
                         </div>
                         <div className="flex space-x-4 items-center">
-                            <li><button className="bg-sky-500 rounded-xl py-2 px-4 font-bold text-sky-100"><a href="/SignUp">Sign Up</a></button></li>
-                            <li><button className="bg-lime-500 rounded-xl py-2 px-4 font-bold text-sky-100"><a href="/SignIn">Sign In</a></button></li>
+                            {isUserLoggedIn ? (
+                                <>
+                                    <li><button className="bg-sky-500 rounded-xl py-2 px-4 font-bold text-sky-100"><a href="/Profile">Profile</a></button></li>
+                                    <li><button className="bg-lime-500 rounded-xl py-2 px-4 font-bold text-sky-100" onClick={() => setIsUserLoggedIn(false)}>Log Out</button></li>
+                                </>
+                            ) : (
+                                <>
+                                    <li><button className="bg-sky-500 rounded-xl py-2 px-4 font-bold text-sky-100"><a href="/SignUp">Sign Up</a></button></li>
+                                    <li><button className="bg-lime-500 rounded-xl py-2 px-4 font-bold text-sky-100"><a href="/SignIn">Sign In</a></button></li>
+                                </>
+                            )}
                         </div>
                     </div>
                 </ul>
             </nav>
-        </header >
+        </header>
     );
 }
