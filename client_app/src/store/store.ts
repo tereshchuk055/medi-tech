7b41321d7a4700f2e11a34e8bafa49e88127ec8c
ipz214_shchss <ipz214_shchss@student.ztu.edu.ua>
2023-12-07 16:24:22 +0200

    Redux initial

---
+++ client_app/src/store/store.ts
@@ -0,0 +1,14 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { news } from './slices/news'
+
+const rootReducer = combineReducers({
+    news: news
+})
+
+export const store = configureStore({
+    reducer: rootReducer,
+})
+
+export type AppDispatch = typeof store.dispatch
+
+export type RootState = ReturnType<typeof store.getState>
